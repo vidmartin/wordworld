773bc4575e5445417832b3582246573a5e347278
vidmartin <martin.vidmar@seznam.cz>
2021-07-09 01:58:54 +0200

    added some files forgotten in last commit

---
+++ WordWorldWebApp/wwwroot/js/wordworld/gameStarter.js
@@ -0,0 +1,7 @@
+﻿
+function startGame() {
+    let username = document.getElementsByName("username")[0].value;
+    let board = document.getElementsByName("board")[0].value;
+
+    window.location.href = `/play/${board}?username=${username}`;
+}
